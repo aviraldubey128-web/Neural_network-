d30984a152d9d9766ee31760c4e0929fc3e5b945
aviral dubey <aviraldubey128@gmail.com>
2026-06-13 15:13:27 +0530

    Update neural-1.py

--- neural-1.py
+++ neural-1.py
@@ -1,2 +1,13 @@
+inputs = [1.2, 5.1,2.1,3.5]
+weights1 = [1.2, 5.1 ,8.7,2.4]
+weights2 = [3.4, -5.7,3.5,-1.4]
+weights3 = [-2.4, 4.8,9.1,-5,3]
+bias1 = 3
+bias2= 4
+bias3= 0.4
 
-print("hello")
+output = [inputs[0]*weights1[0] + inputs[1]*weights2[1] + inputs[2]*weights3[2] + bias1,
+          inputs[0]*weights1[0] + inputs[1]*weights2[1] + inputs[2]*weights3[2] + bias2,
+          inputs[0]*weights1[0] + inputs[1]*weights2[1] + inputs[2]*weights3[2] + bias3]
+output
+
